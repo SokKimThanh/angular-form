588ac4cfddd699a14f7db3d54e1a39e8cfee3fbc
Sok Kim Thanh <thanhsk1991@gmail.com>
2022-02-17 13:59:37 +0700

    update filtering table

--- src/app/practice-reactive-form/filtering-table/filtering-table.component.ts
+++ src/app/practice-reactive-form/filtering-table/filtering-table.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
+import { SelectionModel } from '@angular/cdk/collections';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { MatTableDataSource } from '@angular/material/table';
 
@@ -8,6 +11,10 @@ import { MatTableDataSource } from '@angular/material/table';
   styleUrls: ['./filtering-table.component.scss']
 })
 export class FilteringTableComponent implements OnInit {
+  /* ================================================================= */
+  /* KHU VUC THIET LAP FORM MODEL */
+  /* ================================================================= */
+  autocompleteForm!: FormGroup;
   people = [
     {
       name: 'John',
@@ -34,13 +41,23 @@ export class FilteringTableComponent implements OnInit {
       pet: 'Dog'
     }
   ];
+  tableCols = [
+    { key: 'name', display: 'name' },
+    { key: 'colour', display: 'favouriteColour' },
+    { key: 'id', display: 'id' },
+    { key: 'pet', display: 'pet' }
+  ]
+  get keys() { return this.tableCols.map(({ key }) => key); }
+  selection = new SelectionModel<any>(true, []);
+  @Output('outSelectedRow') outSelectedRow = new EventEmitter<any>();
+  inputFilterList!: string;
+  onSelectedRow(selectedRow: any): void {
+    this.selection.clear();
+    this.selection.toggle(selectedRow);
+    this.outSelectedRow.emit(selectedRow);
+  }
 
-  nameFilter = new FormControl('');
-  idFilter = new FormControl('');
-  colourFilter = new FormControl('');
-  petFilter = new FormControl('');
   dataSource = new MatTableDataSource();
-  columnsToDisplay = ['name', 'id', 'favouriteColour', 'pet'];
   filterValues = {
     name: '',
     id: '',
@@ -48,37 +65,47 @@ export class FilteringTableComponent implements OnInit {
     pet: ''
   };
 
-  constructor() {
+  constructor(private fb: FormBuilder) {
+    this.autocompleteForm = this.fb.group({
+      name: '',
+      id: '',
+      colour: '',
+      pet: '',
+    })
     this.dataSource.data = this.people;
     this.dataSource.filterPredicate = this.createFilter();
   }
 
   ngOnInit() {
-    this.nameFilter.valueChanges
+    this.autocompleteForm.get('name')?.valueChanges
       .subscribe(
         (name: string) => {
           this.filterValues.name = name;
+          this.inputFilterList = name;
           this.dataSource.filter = JSON.stringify(this.filterValues);
         }
       )
-    this.idFilter.valueChanges
+    this.autocompleteForm.get('id')?.valueChanges
       .subscribe(
         (id: string) => {
           this.filterValues.id = id;
+          this.inputFilterList = id;
           this.dataSource.filter = JSON.stringify(this.filterValues);
         }
       )
-    this.colourFilter.valueChanges
+    this.autocompleteForm.get('colour')?.valueChanges
       .subscribe(
         (colour: string) => {
           this.filterValues.colour = colour;
+          this.inputFilterList = colour;
           this.dataSource.filter = JSON.stringify(this.filterValues);
         }
       )
-    this.petFilter.valueChanges
+    this.autocompleteForm.get('pet')?.valueChanges
       .subscribe(
         (pet: string) => {
           this.filterValues.pet = pet;
+          this.inputFilterList = pet;
           this.dataSource.filter = JSON.stringify(this.filterValues);
         }
       )
@@ -93,5 +120,32 @@ export class FilteringTableComponent implements OnInit {
         && data.pet.toLowerCase().indexOf(searchTerms.pet) !== -1;
     }
     return filterFunction;
+  }
+  showBooleanClass(column: any): any {
+    let result = '';
+    if (column.isClass) {
+      if (column.isArrayClass) {
+        for (const key of column.class) {
+          result += key ? `${key ? key : ''},` : `${key ? key : ''},`.substring(0, result.length - 1);
+        }
+        return result.substring(0, result.length - 1);
+      }
+      else {
+        result = column.class;
+        return result;
+      }
+    }
+    if (column.config && column.config.isClass) {
+      if (column.config.isArrayClass) {
+        for (const key of column.config.class) {
+          result += key ? `${key ? key : ''},` : `${key ? key : ''},`.substring(0, result.length - 1);
+        }
+        return result.substring(0, result.length - 1);
+      }
+      else {
+        result = column.config.class;
+        return result;
+      }
+    }
   }
 }

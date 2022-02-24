069ef403f2ad02a86f732886e4852bd6c64ce26c
Sok Kim Thanh <thanhsk1991@gmail.com>
2022-02-24 11:15:30 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router';
-
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { PracticeReactiveFormModule } from './practice-reactive-form/practice-reactive-form.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSelectFilterModule } from 'mat-select-filter';
-import { DesignModule } from './design/design.module';
 
 export const ROUTES: Routes = [
   { path: '', loadChildren: () => import('./practice-reactive-form/practice-reactive-form.module').then(m => m.PracticeReactiveFormModule) }
@@ -22,8 +18,6 @@ export const ROUTES: Routes = [
     ReactiveFormsModule,
     PracticeReactiveFormModule,
     RouterModule.forRoot(ROUTES),
-    BrowserAnimationsModule,
-    MatSelectFilterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/practice-reactive-form/practice-reactive-form.module.ts
+++ src/app/practice-reactive-form/practice-reactive-form.module.ts
@@ -1,23 +1,23 @@
-import { OverlayEffectTableComponent } from './overlay-effect-table/overlay-effect-table.component';
-import { OverlayEffectPaginatorComponent } from './overlay-effect-paginator/overlay-effect-paginator.component';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { PracticeReactiveFormComponent } from './practice-reactive-form.component';
-import { RouterModule } from '@angular/router';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AutocompleteReactiveFormComponent } from './autocomplete-reactive-form/autocomplete-reactive-form.component';
-import { MaterialModule } from '../material/material.module';
-import { TableReactiveFormComponent } from './table-reactive-form/table-reactive-form.component';
-import { FilteringTableComponent } from './filtering-table/filtering-table.component';
-import { FilterOnMatOptionComponent } from './filter-on-mat-option/filter-on-mat-option.component';
-import { MatSelectFilterModule } from 'mat-select-filter';
-import { MatSelectFormComponent } from './mat-select-form/mat-select-form.component';
-import { MatSelectCustomComponent } from './mat-select-custom/mat-select-custom.component';
-import { ParenthesizePipePipe } from './mat-select-custom/parenthesize-pipe.pipe';
-import { FilterProgressBarComponent } from './filter-progress-bar/filter-progress-bar.component';
-import { OverlayEffectComponent } from './overlay-effect/overlay-effect.component';
-import { DesignModule } from '../design/design.module';
-import { ClipboardModule } from 'ngx-clipboard';
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { MatSelectFilterModule } from "mat-select-filter";
+import { ClipboardModule } from "ngx-clipboard";
+import { MaterialModule } from "../material/material.module";
+import { AutocompleteReactiveFormComponent } from "./autocomplete-reactive-form/autocomplete-reactive-form.component";
+import { FilterOnMatOptionComponent } from "./filter-on-mat-option/filter-on-mat-option.component";
+import { FilterProgressBarComponent } from "./filter-progress-bar/filter-progress-bar.component";
+import { FilteringTableComponent } from "./filtering-table/filtering-table.component";
+import { MatSelectCustomComponent } from "./mat-select-custom/mat-select-custom.component";
+import { ParenthesizePipePipe } from "./mat-select-custom/parenthesize-pipe.pipe";
+import { MatSelectFormComponent } from "./mat-select-form/mat-select-form.component";
+import { OverlayEffectPaginatorComponent } from "./overlay-effect-paginator/overlay-effect-paginator.component";
+import { OverlayEffectTableComponent } from "./overlay-effect-table/overlay-effect-table.component";
+import { OverlayEffectComponent } from "./overlay-effect/overlay-effect.component";
+import { PracticeReactiveFormComponent } from "./practice-reactive-form.component";
+import { TableReactiveFormComponent } from "./table-reactive-form/table-reactive-form.component";
+
 @NgModule({
   declarations: [
     PracticeReactiveFormComponent,
@@ -52,7 +52,6 @@ import { ClipboardModule } from 'ngx-clipboard';
     ]),
     MaterialModule,
     MatSelectFilterModule,
-    DesignModule,
     ClipboardModule
   ]
 })

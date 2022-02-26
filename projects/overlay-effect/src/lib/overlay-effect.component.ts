e34cdbcf8477f3392db6460c12404757f8f8bce9
Sok Kim Thanh <thanhsk1991@gmail.com>
2022-02-26 08:31:28 +0700

    update version

---
+++ projects/overlay-effect/src/lib/overlay-effect.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-overlay-effect',
+  template: `
+    <p>
+      overlay-effect works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class OverlayEffectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ projects/overlay-effect/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of overlay-effect
+ */
+
+export * from './lib/overlay-effect.service';
+export * from './lib/overlay-effect.component';
+export * from './lib/overlay-effect.module';
